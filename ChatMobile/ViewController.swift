012c925005258322f84a6093f10abdc7b615be9c
amirhoseyn221177 <amirhoseyn221177>
2021-06-23 00:10:02 -0700

    Initial Commit

---
+++ ChatMobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatMobile
+//
+//  Created by Amir Sayyar on 2021-06-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
